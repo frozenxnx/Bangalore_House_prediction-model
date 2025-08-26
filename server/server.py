65c18e3c13993c67cd1b8e2aaf138f7afe6bb149
AadityaSRV <adityasrivastava9567@gmail.com>
2025-08-26 15:16:16 +0530

    commit changes

--- server/server.py
+++ server/server.py
@@ -1,12 +1,21 @@
-from flask import Flask ,request,jsonify
+from flask import Flask, request, jsonify
 import util
+import os
+
 app = Flask(__name__)
 
+# Load model artifacts once when the server starts
 util.load_saved_artifacts()
+
+# Root route for Render health check
+@app.route("/")
+def index():
+    return "House Price Prediction API is running!", 200
+
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations':util.get_location_names()
+        'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
@@ -19,14 +28,13 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 
-
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting Flask server on port {port}")
+    app.run(host="0.0.0.0", port=port)
